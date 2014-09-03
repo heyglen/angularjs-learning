17efa145d49d605b8c4e9788d018d85e4708e377
Glen <glencharmon@gmail.com>
2014-09-03 21:21:53 +0200

    Video 6

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-angular.module('app', []);
+var app = angular.module('app', ['controllers']);
+
+angular.module('controllers', []).controller('MainController', function($scope) {
+	$scope.myarray = [1, 2, 3, 4]
+
+})
